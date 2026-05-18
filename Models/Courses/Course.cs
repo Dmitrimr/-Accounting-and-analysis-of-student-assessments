f19a4a32465ba434341275e132f912a062ef5913
Dmitrimr <140020639+Dmitrimr@users.noreply.github.com>
2026-05-18 10:56:16 +0300

    Add files via upload

---
+++ Models/Courses/Course.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebAPI.Models.Students;
+using WebAPI.Models.Teachers;
+
+namespace WebAPI.Models.Courses
+{
+    public class Course
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int? TeacherId { get; set; }
+
+        [ForeignKey("TeacherId")]
+        public Teacher? Teacher { get; set; }
+        public virtual ICollection<Student> Students { get; set; } = new List<Student>();
+    }
+}
