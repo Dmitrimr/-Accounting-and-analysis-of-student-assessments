89c3882a484c50d0e8d4f7a31826097218387b30
Dmitrimr <140020639+Dmitrimr@users.noreply.github.com>
2026-05-18 10:58:03 +0300

    Add files via upload

---
+++ Models/Users/User.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models.Users
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public bool IsActive { get; set; } = true;
+    }
+}
